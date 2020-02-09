76616464b5fcc209bbfc79cd12589a0c3f84ebf9
Kirill Rakhman <kirill.rakhman@gmail.com>
2020-02-09 21:10:41 +0100

    clean up

--- app/src/main/java/com/cypressworks/mensaplan/food/likes/LikeStatus.java
+++ app/src/main/java/com/cypressworks/mensaplan/food/likes/LikeStatus.java
@@ -1,10 +1,11 @@
 package com.cypressworks.mensaplan.food.likes;
 
-import androidx.annotation.IntDef;
 import com.cypressworks.mensaplan.food.Meal;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+
+import androidx.annotation.IntDef;
 /**
  * Represents the like state of a {@link Meal}.
  * Realised with integer states instead of an enum because enums
@@ -13,7 +14,7 @@ import java.lang.annotation.RetentionPolicy;
 @Retention(RetentionPolicy.SOURCE)
 @IntDef({LikeStatus.LIKED, LikeStatus.DISLIKED, LikeStatus.NO_LIKE_INFO})
 public @interface LikeStatus {
-    public static final int LIKED = 1;
-    public static final int DISLIKED = -1;
-    public static final int NO_LIKE_INFO = 0;
+    int LIKED = 1;
+    int DISLIKED = -1;
+    int NO_LIKE_INFO = 0;
 }
